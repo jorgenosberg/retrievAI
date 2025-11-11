759311841ed84946df7ffe63948bfc35b498d296
Jørgen Andresen Osberg <jorgen.osberg@gmail.com>
2025-11-12 00:36:59 +0100

    Remove legacy artifacts from repo

--- retrievai/run_app.py
+++
@@ -1,9 +0,0 @@
-import sys
-from streamlit.web import cli as stcli
-
-def main():
-    sys.argv = ["streamlit", "run", "./retrievai/app.py"]
-    sys.exit(stcli.main())
-
-if __name__ == "__main__":
-    main()
